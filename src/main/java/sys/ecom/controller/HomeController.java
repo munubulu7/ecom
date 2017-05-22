96c57f011ac06d54a2fa55f5993766fbf81c8c7d
My System <My System@DESKTOP-JGI8675>
2017-05-22 22:58:08 +0530

    404 not found page mapping

--- src/main/java/sys/ecom/controller/HomeController.java
+++ src/main/java/sys/ecom/controller/HomeController.java
@@ -1,6 +1,7 @@
 package sys.ecom.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -8,5 +9,18 @@ public class HomeController {
 	@GetMapping("/app")
 	public String index() {
 		return "index";
+	}
+
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public String error404() {
+		return "404";
+	}
+
+	public class ResourceNotFoundException extends RuntimeException {
+
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = 1L;
 	}
 }
