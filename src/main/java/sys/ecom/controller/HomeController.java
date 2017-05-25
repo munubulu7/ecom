fcab44ac4a9b69eeea3052b3ce675dd75cb18dbc
munubulu7 <munubulu7@gmail.com>
2017-05-25 06:59:44 +0530

    merge fix

--- src/main/java/sys/ecom/controller/HomeController.java
+++ src/main/java/sys/ecom/controller/HomeController.java
@@ -1,92 +1,128 @@
 package sys.ecom.controller;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
+import sys.ecom.components.*;
 
-import sys.ecom.components.Brand;
-import sys.ecom.components.Catgory;
-import sys.ecom.components.Menu;
-import sys.ecom.components.SubCategory;
-import sys.ecom.components.SubMenu;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 public class HomeController {
-	@GetMapping("/app")
-	public ModelAndView index() {
-		ModelAndView view = new ModelAndView("index");
+    @GetMapping("/app")
+    public ModelAndView index() {
+        ModelAndView view = new ModelAndView("index");
 
-		List<Menu> menus = new ArrayList<Menu>();
-		for (int i = 0; i < 5; i++) {
-			Menu menu = new Menu();
-			menu.setName("Name" + i);
-			menu.setDisplayName("DispName" + i);
-			menu.setActive(i == 3 ? false : true);
-			menu.setTergetLink("#");
-			menu.setSubMenus(new ArrayList<SubMenu>());
-			if (i != 2) {
-				for (int j = 0; j < 5; j++) {
-					SubMenu subMenu = new SubMenu();
-					subMenu.setName("Name" + j);
-					subMenu.setDisplayName("DispSubMenuName" + j);
-					subMenu.setActive(i == j ? false : true);
-					subMenu.setTergetLink("#");
-					menu.getSubMenus().add(subMenu);
-				}
-			}
-			menus.add(menu);
-		}
-		view.addObject("menus", menus);
+        List<Menu> menus = new ArrayList<Menu>();
+        for (int i = 0; i < 5; i++) {
+            Menu menu = new Menu();
+            menu.setName("Name" + i);
+            menu.setDisplayName("DispName" + i);
+            menu.setActive(i == 3 ? false : true);
+            menu.setTergetLink("#");
+            menu.setSubMenus(new ArrayList<SubMenu>());
+            if (i != 2) {
+                for (int j = 0; j < 5; j++) {
+                    SubMenu subMenu = new SubMenu();
+                    subMenu.setName("Name" + j);
+                    subMenu.setDisplayName("DispSubMenuName" + j);
+                    subMenu.setActive(i == j ? false : true);
+                    subMenu.setTergetLink("#");
+                    menu.getSubMenus().add(subMenu);
+                }
+            }
+            menus.add(menu);
+        }
+        view.addObject("menus", menus);
 
-		List<Catgory> catgories = new ArrayList<Catgory>();
-		for (int i = 0; i < 5; i++) {
-			Catgory catgory = new Catgory();
-			catgory.setName("Name" + i);
-			catgory.setDisplayName("CategoryName" + i);
-			catgory.setActive(i == 3 ? false : true);
-			catgory.setTargetId("link"+i);
-			catgory.setTergetLink("#"+catgory.getTargetId());
-			catgory.setSubCategories(new ArrayList<SubCategory>());
-			if (i != 2) {
-				for (int j = 0; j < 5; j++) {
-					SubCategory subCategory = new SubCategory();
-					subCategory.setName("Name" + j);
-					subCategory.setDisplayName("SubCatName" + j);
-					subCategory.setActive(i == j ? false : true);
-					subCategory.setTergetLink("#");
-					catgory.getSubCategories().add(subCategory);
-				}
-			}
-			catgories.add(catgory);
-		}
-		view.addObject("catgories", catgories);
-		
-		List<Brand> brands = new ArrayList<Brand>();
-		for (int i = 0; i < 5; i++) {
-			Brand brand = new Brand();
-			brand.setName("Name" + i);
-			brand.setDisplayName("Brand " + i);
-			brand.setActive(i == 3 ? false : true);
-			brand.setTergetLink("#");
-			brand.setQuantity(5);
-			
-			brands.add(brand);
-		}
-		view.addObject("brands", brands);
+        List<Catgory> catgories = new ArrayList<Catgory>();
+        for (int i = 0; i < 5; i++) {
+            Catgory catgory = new Catgory();
+            catgory.setName("Name" + i);
+            catgory.setDisplayName("CategoryName" + i);
+            catgory.setActive(i == 3 ? false : true);
+            catgory.setTargetId("link" + i);
+            catgory.setTergetLink("#" + catgory.getTargetId());
+            catgory.setSubCategories(new ArrayList<SubCategory>());
+            if (i != 2) {
+                for (int j = 0; j < 5; j++) {
+                    SubCategory subCategory = new SubCategory();
+                    subCategory.setName("Name" + j);
+                    subCategory.setDisplayName("SubCatName" + j);
+                    subCategory.setActive(i == j ? false : true);
+                    subCategory.setTergetLink("#");
+                    catgory.getSubCategories().add(subCategory);
+                }
+            }
+            catgories.add(catgory);
+        }
+        view.addObject("catgories", catgories);
 
-		return view;
-	}
+        List<Brand> brands = new ArrayList<Brand>();
+        for (int i = 0; i < 5; i++) {
+            Brand brand = new Brand();
+            brand.setName("Name" + i);
+            brand.setDisplayName("Brand " + i);
+            brand.setActive(i == 3 ? false : true);
+            brand.setTergetLink("#");
+            brand.setQuantity(5);
 
-	@ExceptionHandler(ResourceNotFoundException.class)
-	public String error404() {
-		return "404";
-	}
+            brands.add(brand);
+        }
+        view.addObject("brands", brands);
 
-	public class ResourceNotFoundException extends RuntimeException {
-		private static final long serialVersionUID = 1L;
-	}
+        List<Item> items = new ArrayList<Item>();
+        Item item = new Item();
+        item.setName("E-SHOPPER");
+        item.setTagLine("Free E-Commerce Template");
+        item.setDescription("Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ");
+        item.setBtnText("Get it now");
+        item.setPriceTagImageUrl("/resources/images/home/pricing.png");
+        item.setImageUrls(Arrays.asList(new ItemImageUrl("/resources/images/home/girl1.jpg")));
+        items.add(item);
+
+        Item item1 = new Item();
+        item1.setName("E-SHOPPER");
+        item1.setTagLine("Free E-Commerce Template");
+        item1.setDescription("Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ");
+        item1.setBtnText("Get it now");
+        item1.setPriceTagImageUrl("/resources/images/home/pricing.png");
+        item1.setImageUrls(Arrays.asList(new ItemImageUrl("/resources/images/home/girl2.jpg")));
+        items.add(item1);
+
+        Item item2 = new Item();
+        item2.setName("E-SHOPPER");
+        item2.setTagLine("Free E-Commerce Template");
+        item2.setDescription("Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ");
+        item2.setBtnText("Get it now");
+        item2.setPriceTagImageUrl("/resources/images/home/pricing.png");
+        item2.setImageUrls(Arrays.asList(new ItemImageUrl("/resources/images/home/girl3.jpg")));
+        items.add(item2);
+
+        Item item3 = new Item();
+        item3.setName("E-SHOPPER");
+        item3.setTagLine("Free E-Commerce Template");
+        item3.setDescription("Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ");
+        item3.setBtnText("Get it now");
+        item3.setPriceTagImageUrl("/resources/images/home/pricing.png");
+        item3.setImageUrls(Arrays.asList(new ItemImageUrl("/resources/images/home/girl3.jpg")));
+        items.add(item3);
+
+//		view.addObject("items", null);
+        view.addObject("items", items);
+
+        return view;
+    }
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public String error404() {
+        return "404";
+    }
+
+    public class ResourceNotFoundException extends RuntimeException {
+        private static final long serialVersionUID = 1L;
+    }
 }
--- src/main/webapp/WEB-INF/views/index.jsp
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -164,94 +164,71 @@
 	</header>
 	<!--/header-->
 
-	<section id="slider">
-		<!--slider-->
-		<div class="container">
-			<div class="row">
-				<div class="col-sm-12">
-					<div id="slider-carousel" class="carousel slide"
-						data-ride="carousel">
-						<ol class="carousel-indicators">
-							<li data-target="#slider-carousel" data-slide-to="0"
-								class="active"></li>
-							<li data-target="#slider-carousel" data-slide-to="1"></li>
-							<li data-target="#slider-carousel" data-slide-to="2"></li>
-						</ol>
+	<c:if test="${items.size()!=null&&items.size()>0}">
+    <section id="slider">
+        <!--slider-->
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12">
+                    <div id="slider-carousel" class="carousel slide"
+                         data-ride="carousel">
+                        <c:set var="i" value="0"/>
+                        <ol class="carousel-indicators">
+                            <c:forEach items="${items}">
+                                <c:choose>
+                                    <c:when test="${i==0}">
+                                        <li data-target="#slider-carousel" data-slide-to="0"
+                                            class="active"></li>
+                                        <c:set var="i" value="1"/>
+                                    </c:when>
+                                    <c:otherwise>
+                                        <li data-target="#slider-carousel" data-slide-to="1"></li>
+                                    </c:otherwise>
+                                </c:choose>
+                            </c:forEach>
+                        </ol>
 
-						<div class="carousel-inner">
-							<div class="item active">
-								<div class="col-sm-6">
-									<h1>
-										<span>E</span>-SHOPPER
-									</h1>
-									<h2>Free E-Commerce Template</h2>
-									<p>Lorem ipsum dolor sit amet, consectetur adipisicing
-										elit, sed do eiusmod tempor incididunt ut labore et dolore
-										magna aliqua.</p>
-									<button type="button" class="btn btn-default get">Get
-										it now</button>
-								</div>
-								<div class="col-sm-6">
-									<img src="/resources/images/home/girl1.jpg"
-										class="girl img-responsive" alt="" /> <img
-										src="/resources/images/home/pricing.png" class="pricing"
-										alt="" />
-								</div>
-							</div>
-							<div class="item">
-								<div class="col-sm-6">
-									<h1>
-										<span>E</span>-SHOPPER
-									</h1>
-									<h2>100% Responsive Design</h2>
-									<p>Lorem ipsum dolor sit amet, consectetur adipisicing
-										elit, sed do eiusmod tempor incididunt ut labore et dolore
-										magna aliqua.</p>
-									<button type="button" class="btn btn-default get">Get
-										it now</button>
-								</div>
-								<div class="col-sm-6">
-									<img src="/resources/images/home/girl2.jpg"
-										class="girl img-responsive" alt="" /> <img
-										src="/resources/images/home/pricing.png" class="pricing"
-										alt="" />
-								</div>
-							</div>
+                        <div class="carousel-inner">
+                            <c:set var="i" value="0"/>
+                            <c:forEach items="${items}" var="item">
+                                <div class="item ${i==0?'active':""}">
+                                    <div class="col-sm-6">
+                                        <h1>
+                                            <span>${item.name}</span>
+                                        </h1>
+                                        <h2>${item.tagLine}</h2>
+                                        <p>${item.description}</p>
+                                        <button type="button" class="btn btn-default get">Get
+                                                ${item.btnText}
+                                        </button>
+                                    </div>
+                                    <div class="col-sm-6">
+                                        <c:forEach items="${item.imageUrls}" var="imageUrl">
+                                            <c:set var="image" value="${imageUrl.imageURL}"/>
+                                        </c:forEach>
+                                        <img src="${item.imageUrls.get(0).imageURL}"
+                                             class="girl img-responsive" alt=""/> <img
+                                            src="${item.priceTagImageUrl}" class="pricing"
+                                            alt=""/>
+                                    </div>
+                                </div>
+                                <c:set var="i" value="1"/>
+                            </c:forEach>
+                        </div>
 
-							<div class="item">
-								<div class="col-sm-6">
-									<h1>
-										<span>E</span>-SHOPPER
-									</h1>
-									<h2>Free Ecommerce Template</h2>
-									<p>Lorem ipsum dolor sit amet, consectetur adipisicing
-										elit, sed do eiusmod tempor incididunt ut labore et dolore
-										magna aliqua.</p>
-									<button type="button" class="btn btn-default get">Get
-										it now</button>
-								</div>
-								<div class="col-sm-6">
-									<img src="/resources/images/home/girl3.jpg"
-										class="girl img-responsive" alt="" /> <img
-										src="/resources/images/home/pricing.png" class="pricing"
-										alt="" />
-								</div>
-							</div>
+                        <a href="#slider-carousel" class="left control-carousel hidden-xs"
+                           data-slide="prev"> <i class="fa fa-angle-left"></i>
+                        </a> <a href="#slider-carousel"
+                                class="right control-carousel hidden-xs" data-slide="next"> <i
+                            class="fa fa-angle-right"></i>
+                    </a>
+                    </div>
 
-						</div>
-
-						<a href="#slider-carousel" class="left control-carousel hidden-xs"
-							data-slide="prev"> <i class="fa fa-angle-left"></i>
-						</a> <a href="#slider-carousel"
-							class="right control-carousel hidden-xs" data-slide="next"> <i
-							class="fa fa-angle-right"></i>
-						</a>
-					</div>
-
-				</div>
-			</div>
-		</div>
-	</section>
+                </div>
+            </div>
+        </div>
+    </section>
+</c:if>
 	<!--/slider-->
 
 	<section>
