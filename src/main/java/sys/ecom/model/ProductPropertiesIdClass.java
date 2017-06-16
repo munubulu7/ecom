eb566bb8f9d5bc035090dc03776eca40ef31413b
Debashis Paul <munubulu7@gmail.com>
2017-06-16 21:23:53 +0530

    equals and hashCode override in ProductPropertiesIdClass

--- src/main/java/sys/ecom/model/ProductPropertiesIdClass.java
+++ src/main/java/sys/ecom/model/ProductPropertiesIdClass.java
@@ -6,6 +6,38 @@ import java.io.Serializable;
  * Created by My System on 6/7/2017.
  */
 public class ProductPropertiesIdClass implements Serializable {
-    private long propertiesId;
-    private long productId;
+    private Long propertiesId;
+    private Long productId;
+
+
+
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result
+                + ((propertiesId == null) ? 0 : propertiesId.hashCode());
+        result = (int) (prime * result + productId);
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        ProductPropertiesIdClass other = (ProductPropertiesIdClass) obj;
+        if (propertiesId == null) {
+            if (other.propertiesId != null)
+                return false;
+        } else if (!propertiesId.equals(other.propertiesId))
+            return false;
+        if (productId != other.productId)
+            return false;
+        return true;
+    }
 }
